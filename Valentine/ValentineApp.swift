cb3d2969871002cd88a90fd8377d73315f58c19f
Jesus Chapman <jesuschapmandev@outlook.com>
2026-06-16 19:28:36 -0500

    Initial Commit

---
+++ Valentine/ValentineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValentineApp.swift
+//  Valentine
+//
+//  Created by Jesús David Chapman Vélez on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ValentineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
